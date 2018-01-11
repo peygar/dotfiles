ee55f0d07648e9a8d2493afe46a3803234ebed23
Peyman Gardideh <peyman+github@halfmoon.ws>
2018-01-10 23:16:58 -0800

    fix instal_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,6 +1,6 @@
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 cp ./src/zsh/.zshrc ~/.zshrc
-cp ./src/zsh/.oh-my-zsh/themes/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
-cp -r ./src/zsh/.oh-my-zsh/custom ~/.oh-my-zsh/custom
+cp ./src/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
+cp -r ./src/zsh/custom ~/.oh-my-zsh/custom
 cp -r ./src/zsh/.bin ~/.bin
