aacc757fe3b30d35b88314dc3d237414e635be30
Peyman Gardideh <peyman+github@halfmoon.ws>
2018-01-10 23:27:59 -0800

    get it together

--- install_iterm.sh
+++ install_iterm.sh
@@ -1,3 +1,3 @@
 mkdir ~/Library/Application\ Support/iTerm2/Configuration
-cp -r ./src/iterm/Configuration ~/Library/Application\ Support/iTerm2/Configuration
+cp -r ./src/iterm/Configuration ~/Library/Application\ Support/iTerm2/
 echo "Go to iterm2 and put '~/Library/Application\ Support/iTerm2/Configuration' in preferences custom folder"
